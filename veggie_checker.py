94949eb75d5c821891a3b9a96d05496ccde95731
Dylan Pham <32346117+dylan-pham@users.noreply.github.com>
2019-07-28 22:29:54 -0400

    Update veggie_checker.py

--- veggie_checker.py
+++ veggie_checker.py
@@ -4,12 +4,15 @@ from PIL import Image
 
 img = Image.open('images/tush.jpg')
 img = np.array(img)
-ir, ir_n_g, ir_n_r = cv.split(img)
+ir, ir_n_g, ir_n_r = cv.split(img) # splitting into different color channels
+
+# without red filter: b, g, r
+# with red filter: ir, ir + green, ir + red
 
 bottom = ir_n_r
 bottom[bottom == 0] = 0.01
 
-ndvi = ((ir + ir) - ir_n_r)/(bottom)
+ndvi = (ir + ir - ir_n_r)/(bottom)
 
 while True:
     cv.imshow('', ndvi)
